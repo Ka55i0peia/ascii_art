e38385f78e1e3ebf43f35a1d021f7f5fea96ce21
jagi <jan.gietzel@gmail.com>
2023-01-08 15:47:30 +0100

    add pylint as one linter to the pipeline and add some doc where it was missing

--- draw_ascii/main.py
+++ draw_ascii/main.py
@@ -10,6 +10,14 @@ def asciinize(
     font: str = "univers",
     width: str = 200,
 ):
+    """Returns ascii version of text.
+
+    :param text: input text, defaults to "hi there!"
+    :param font: font of the ascii art. Check out http://www.figlet.org/examples.html for
+        samples, defaults to "univers"
+    :param width: length of the ascii art, defaults to 200
+    :return: raw text of ascii art
+    """
     ascii_art = figlet_format(text, font, width=width)
     return Response(content=ascii_art, media_type="text/plain")
 
--- tests/main_test.py
+++ tests/main_test.py
@@ -2,4 +2,5 @@ from draw_ascii.main import asciinize
 
 
 def test_no_throw():
+    """simple test to check if program executes"""
     asciinize()
